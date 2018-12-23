46a131ab1bfc606c7c4b6050168be2cbe843b858
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-12-23 13:14:54 -0800

    Add Gradle building

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    java
+    `java-library`
+    maven
+}
+
+dependencies {
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+}
+
+repositories {
+    jcenter()
+}
--- src/test/java/badlog/lib/Main.java
+++ src/test/java/badlog/lib/Main.java
@@ -7,21 +7,17 @@ public class Main {
 			BadLog.createValue("Example Value", System.getProperty("os.version"));
 			
 			BadLog.createTopic("Example Topic", "Bytes", () -> (double) Runtime.getRuntime().freeMemory());
-			
+
 			BadLog.createTopic("Topic with attributes", BadLog.UNITLESS, () -> 3.2, "attr1", "attr2");
 			
 			BadLog.createTopicSubscriber("Subscribed topic", "s", DataInferMode.DEFAULT);
 		}
 		log.finishInitialization();
 		
-		
-		
 		for (int i = 0; i < 10; i++) {
-			
 			// Publish to subscribed topic
 			BadLog.publish("Subscribed topic", (double) System.nanoTime());
-			
-			
+
 			log.updateTopics();
 			log.log();
 		}
